458e7561aad18ed1b2070049fb72e96fab6a3ef2
soykot2910 <ashrafulislam2910@gmail.com>
2021-08-09 14:13:59 +0600

    env add

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Movie from "./Movie";
 
-const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=8f4b770dd036a39a993bd278fa769318&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=`;
-const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=8f4b770dd036a39a993bd278fa769318&language=en-US&query=`;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=`;
 
 export default function MovieList() {
   const [movies, setMovies] = useState([]);
@@ -34,7 +34,6 @@ export default function MovieList() {
         .then((res) => res.json())
         .then((data) => {
           setMovies(data.results);
-          console.log(movies);
         });
     }
   };
